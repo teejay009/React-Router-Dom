4817f2d22576b499992c3c0c469336b5155826c7
teejay009 <tajdjimoh@gmail.com>
2023-11-01 15:59:43 +0100

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p style={{ marginTop: "2rem" }}> This App is is a part of react lesson in DLT Africa</p>
+    </main>
+
+  )
+}
+
+
+export default About
---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Missing from "./Missing";
+import About from "./About";
+import HomeLayout from "./HomeLayout";
+
+const App = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "Rocco",
+      date: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet corrupti facere dignissimos laborum! Numquam autem inventore vel architecto nam iure id placeat? Iste, maxime nisi.",
+    },
+    {
+      id: 2,
+      title: "Kanas",
+      date: "September 21, 2026 12:19:36 AM",
+      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet c.",
+    },
+    {
+      id: 3,
+      title: "Muhammed",
+      date: "July 01, 2024 11:55:36 AM",
+      body: " consectetur adipisicing elit. Amet corrupti facere dignissimos laborum! Numquam autem inventore vel architecto nam iure id placeat? Iste, ",
+    },
+    {
+      id: 4,
+      title: "Supreme",
+      date: "August 03, 2025 13:42:36 AM",
+      body: " dignissimos laborum! Numquam autem inventore vel architecto nam consectetur adipisicing elit. Amet corrupti facere  iure id placeat? Iste, ",
+    },
+  ]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [search, setSearch] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  useEffect(() => {
+    const filterResult = posts.filter(post =>
+       post.body.toLowerCase().includes(search.toLowerCase())
+       || 
+       post.title.toLowerCase().includes(search.toLowerCase()))
+       setSearchResult(filterResult.reverse());
+
+  }, [posts, search])
+
+  const navigate = useNavigate();
+
+  const handleDelete = (id) => {
+    const postList = posts.filter((post) => post.id !== id);
+    setPosts(postList);
+    navigate("/");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const id = posts.length ? posts[posts.length-1].id + 1 : 1
+    const date = format(new Date(), 'MMMM dd, yyyy pp')
+    const newPost = {id, title: postTitle, date, body: postBody }
+    const allPost = [...posts, newPost]
+    setPosts(allPost)
+    setPostTitle('')
+    setPostBody('')
+    navigate('/')
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomeLayout search={search} setSearch={setSearch} />}
+      >
+        <Route index element={<Home posts={searchResult} />} />
+        <Route path="/post">
+          <Route
+            index
+            element={
+              <NewPost
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+                handleSubmit={handleSubmit}
+              />
+            }
+          />
+          <Route
+            path=":id"
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
